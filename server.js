27e39e76239f3fd260e373c367392f36b6236af6
Devon Weems <devonweems@Devons-MacBook-Air.local>
2020-10-21 18:59:14 -0400

    heroku deploy

--- server.js
+++ server.js
@@ -15,6 +15,8 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
+mongodb+srv://DWeems:canada1@cluster0.zbfxl.mongodb.net/cluster0?retryWrites=true&w=majority
+
 mongoose.connect("mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false
